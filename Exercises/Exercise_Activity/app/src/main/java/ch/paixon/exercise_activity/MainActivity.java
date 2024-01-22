a2089d65a5ea5ff14c7cba6affa0edd7a133b8cc
Stefan Schöb <stefan.schoeb@paixon.ch>
2024-01-22 15:23:58 +0100

    Working activity exercise

--- Exercises/Exercise_Activity/app/src/main/java/ch/paixon/exercise_activity/MainActivity.java
+++ Exercises/Exercise_Activity/app/src/main/java/ch/paixon/exercise_activity/MainActivity.java
@@ -1,10 +1,7 @@
 package ch.paixon.exercise_activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
