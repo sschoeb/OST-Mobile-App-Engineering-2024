ec2a4f918848019305d50383f927fa73202c5889
Stefan Schöb <stefan.schoeb@paixon.ch>
2024-01-22 17:54:48 +0100

    Update all / add solutions for activity and intent

--- Exercises/Exercise_Network/app/src/main/java/ch/paixon/exercise_network/MainActivity.java
+++ Exercises/Exercise_Network/app/src/main/java/ch/paixon/exercise_network/MainActivity.java
@@ -1,31 +1,32 @@
 package ch.paixon.exercise_network;
 
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.originTime)
     public TextView originTime;
 
-    @BindView(R.id.destinationTime)
     public TextView destinationTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        originTime = findViewById(R.id.originTime);
+        destinationTime = findViewById(R.id.destinationTime);
+
+        Button searchButton = findViewById(R.id.search);
+        searchButton.setOnClickListener((View view) -> {
+            onSearchClick();
+        });
     }
 
-    @OnClick(R.id.search)
     public void onSearchClick() {
         // TODO 1: Verwende Retrofit.Builder um eine Retrofit Instanz zu erstellen
         // TODO 2: Erstelle einen RetroFit Service sowie passende DTO-Klassen welcher das Connection-API representiert
--- Exercises/Exercise_Testing/app/src/androidTest/java/ch/paixon/exercise_testing/MainActivityTests.java
+++ Exercises/Exercise_Testing/app/src/androidTest/java/ch/paixon/exercise_testing/MainActivityTests.java
@@ -1,26 +1,23 @@
 package ch.paixon.exercise_testing;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTests {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityRule = new ActivityScenarioRule (MainActivity.class);
 
     @Test
     public void addDoesAdd1OnCounterValue() {
